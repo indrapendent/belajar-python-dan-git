f9b236d2fe1c6a7b37d60a33eb076d01d98b9187
indrapendent <indrapper11@gmail.com>
2025-09-04 23:07:04 +0700

    pertama kali commit

---
+++ Test belajar Python.py
@@ -0,0 +1,19 @@
+# String
+nama = 'Iqbal'
+# Int
+umur = 20
+# Float
+tinggi = 168.7
+# Boolean
+statusMenikah = False
+punyasepeda = False
+# Aritmatika
+a = 1
+b = 2.5
+c = a + b
+print(f"Nama: {nama}")
+print(umur)
+print(tinggi)
+print(f"Status: {'Sudah Kawin' if statusMenikah else 'Belum Kawin'}")
+print(f"punya sepeda ngga: {'punya dong' if punyasepeda else 'belum beli'}")
+print(c)
